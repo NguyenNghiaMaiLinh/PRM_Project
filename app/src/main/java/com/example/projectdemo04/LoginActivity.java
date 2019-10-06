4a787fba33b2afab33b451cf06c632eebfc260b5
giangtct <koingok293@gmail.com>
2019-10-06 17:12:14 +0700

    Gian commit code login fb

--- app/src/main/java/com/example/projectdemo04/LoginActivity.java
+++ app/src/main/java/com/example/projectdemo04/LoginActivity.java
@@ -3,11 +3,11 @@ package com.example.projectdemo04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
@@ -16,7 +16,23 @@ import android.widget.Toast;
 import com.example.projectdemo04.model.Token;
 import com.example.projectdemo04.presenters.LoginPresenter;
 import com.example.projectdemo04.views.LoginView;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.kaopiz.kprogresshud.KProgressHUD;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -29,18 +45,91 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     private String baseUrl;
     private LoginPresenter mLoginPresenter;
     private static int REGISTER_ACTIVITY = 1;
-    private String accessToken ;
+    private String accessToken;
     private String tokenType;
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
         userNamel = findViewById(R.id.userName1);
         userPass = findViewById(R.id.userPassword);
         mLoginPresenter = new LoginPresenter(this);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+//        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(
+                "user_friends", "email", "public_profile", "user_gender", "user_location"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                Log.v("LoginActivity Response ", response.toString());
+
+                                try {
+                                    String id = object.getString("id");
+                                    String name = object.getString("name");
+                                    String gender = object.getString("gender");
+                                    String birthday = object.getString("birthday");
+                                    String location = object.getJSONObject("location").getString("name");
+                                    Log.v("id = ", " " + id);
+                                    Log.v("name = ", " " + name);
+                                    Log.v("gender = ", " " + gender);
+                                    Log.v("birthday = ", " " + birthday);
+                                    Log.v("location = ", " " + location);
+
+                                    String email;
+                                    if (object.has("email")) {
+                                        email = object.getString("email");
+                                        Log.v("email = ", " " + email);
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,gender,email, birthday,location");
+                request.setParameters(parameters);
+                request.executeAsync();
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                LoginManager.getInstance().logOut();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        if (isLoggedIn) {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }
     }
+
 
     public void onClickHomePage(View view) {
         final String username = userNamel.getText().toString().trim();
@@ -76,8 +165,8 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         accessToken = token.getAccessToken();
         tokenType = token.getTokenType();
         Bundle bundle = new Bundle();
-        bundle.putString("accessToken",accessToken);
-        bundle.putString("tokenType",tokenType);
+        bundle.putString("accessToken", accessToken);
+        bundle.putString("tokenType", tokenType);
         Toast.makeText(getApplicationContext(), "Thành công", Toast.LENGTH_LONG).show();
         final KProgressHUD kProgressHUD = KProgressHUDManager.showProgessBar(this, "Thành công");
         new Handler().postDelayed(new Runnable() {
@@ -93,6 +182,12 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     @Override
     public void loginFailed(String s) {
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
