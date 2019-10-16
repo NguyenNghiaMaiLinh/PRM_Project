e3426463d9d328a1c409b08c31f677d39738ae0f
hoanthien <hoanthien97@gmail.com>
2019-10-16 08:04:29 +0700

    add discount to Book item_fragment

--- app/src/main/java/com/example/projectdemo04/home/BookRecyclerAdapter.java
+++ app/src/main/java/com/example/projectdemo04/home/BookRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.projectdemo04.home;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,13 +23,15 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         private ImageView bookImg;
-        private TextView bookTitle,bookPrice,bookDiscount,bookId;
+        private TextView bookTitle,bookPrice,bookDiscount,bookId,bookOriginalPrice;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             bookImg = itemView.findViewById(R.id.imageBook);
             bookTitle = itemView.findViewById(R.id.txtBookTitle);
             bookPrice = itemView.findViewById(R.id.txtBookPrice);
             bookId = itemView.findViewById(R.id.bookId);
+            bookDiscount = itemView.findViewById(R.id.txtBookDiscount);
+            bookOriginalPrice  = itemView.findViewById(R.id.txtBookOriginalPrice);
         }
 
 
@@ -54,10 +57,20 @@ public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapte
         holder.bookPrice.setText(convertPriceToFormatString(book.getPrice()));
         holder.bookTitle.setText(books.get(position).getProductName()+"");
         holder.bookId.setText(books.get(position).getId()+"");
+        if(book.getDiscount() >0){
+            holder.bookDiscount.setText("-" + book.getDiscount()*100 + "%");
+            holder.bookDiscount.setVisibility(View.VISIBLE);
+            holder.bookOriginalPrice.setText(convertPriceToFormatString(book.getPrice()));
+            holder.bookOriginalPrice.setVisibility(View.VISIBLE);
+            holder.bookOriginalPrice.setPaintFlags(holder.bookOriginalPrice.getPaintFlags()|Paint.STRIKE_THRU_TEXT_FLAG);
+            long sellPrice =Math.round( book.getPrice()*(1-book.getDiscount()));
+            holder.bookPrice.setText(convertPriceToFormatString(sellPrice));
+        }
+
         Picasso.get().load(book.getImgUrl()).into(holder.bookImg);
 
     }
-    private String convertPriceToFormatString(int price){
+    private String convertPriceToFormatString(long price){
         String raw = price +"";
         String result = raw.substring(0,raw.length()-3)+ "." + raw.substring(raw.length()-3, raw.length()) + "đ";
         return result;
